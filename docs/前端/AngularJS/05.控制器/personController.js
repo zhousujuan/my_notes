6ae4027a205ace4806ba70c037af27c56c820d11
zhousujuan <179997445@qq.com>
2022-07-06 11:02:23 +0800

    外部文件中的控制器

---
+++ docs/前端/AngularJS/05.控制器/personController.js
@@ -0,0 +1,10 @@
+angular.module('myApp', [])
+.controller('personController', function(
+    $scope
+    ) {
+    $scope.firstName = "John",
+    $scope.lastName = "Doe",
+    $scope.fullName = function() {
+        return $scope.firstName + " " + $scope.lastName;
+    }
+})
